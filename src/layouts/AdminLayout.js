2d298b7d24e575dae7092a6b5b65d730eaf50e01
maabdullah511 <maabdullah511@gmail.com>
2022-11-29 00:19:04 +0600

    layout desing change

--- src/layouts/AdminLayout.js
+++ src/layouts/AdminLayout.js
@@ -1,24 +1,13 @@
 import React from 'react'
+import '../assets/app-light.css';
 import { useLocation, Route, Router } from "react-router-dom";
 import { createBrowserRouter, redirect, Link, Outlet, useLoaderData, RouterProvider } from "react-router-dom";
 import Notification from '../components/partials/admin/Notification'
 import Sidebar from '../components/partials/admin/Sidebar'
 import Header from '../components/partials/admin/Header'
-import Dashboard from '../views/admin/Dashboard'
 import routes from '../utils/adminMenus';
 
 const AdminLayout = (props) => {
-    const getRoutes = (routes) => {
-        return routes.map((prop, key) => {
-            return (
-                <Route
-                    path={prop.layout + prop.path}
-                    render={(props) => <prop.component {...props} />}
-                    key={key}
-                />
-            );
-        });
-    };
     return (
         <div className="wrapper vertical light">
             <Header />
--- src/layouts/WebLayout.js
+++ src/layouts/WebLayout.js
@@ -1,24 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import '../assets/style.css';
 import { createBrowserRouter, redirect, Link, Outlet, useLoaderData, RouterProvider, useLocation } from "react-router-dom";
-import TopHeader from '../components/partials/web/TopHeader';
 import Header from '../components/partials/web/Header';
 import Footer from '../components/partials/web/Footer';
 import HomeMenu from '../components/partials/web/HomeMenu';
 import Menu from '../components/partials/web/Menu';
-import Search from '../components/partials/web/Search';
-import Breadcrumb from '../components/partials/web/Breadcrumb';
-import Home from '../views/web/Home';
-
 
 const WebLayout = () => {
-    // console.log('current URL 👉️', window.location.href);
-    // console.log('current Pathname 👉️', window.location.pathname);
-
     // 👇️ with React router
     const location = useLocation();
-    // console.log('hash', location.hash);
-    // console.log('pathname', location.pathname);
-    // console.log('search', location.search);
     const { pathName } = useLocation();
 
     useEffect(() => {
@@ -27,8 +17,6 @@ const WebLayout = () => {
 
     return (
         <div>
-            {/* <h2>Current URL 👉️ { window.location.href }</h2>
-            <h2>Current Pathname 👉️ { window.location.pathname }</h2> */}
             <Header />
             {window.location.pathname == '/' ? <HomeMenu /> : <Menu />}
             <Outlet />
