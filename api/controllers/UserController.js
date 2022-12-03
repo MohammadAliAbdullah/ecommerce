d43596f3816c90da74b5046d62f8efa581729b5e
maabdullah511 <maabdullah511@gmail.com>
2022-12-03 23:40:16 +0600

    show all users api

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,1 +1,13 @@
 // user controller
+const db = require('../models');
+const User = db.user;
+const Role = db.role;
+
+exports.users = (req, res) => {
+    User.find({}, function (err, result) {
+        if (err) throw new err();
+        // if (err) return handleError(err);
+        // console.log(result);
+        return res.json(result);
+    })
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,6 @@
 var router = require("express").Router();
 const auth = require('../controllers/AuthController');
+const user = require('../controllers/UserController');
 const duplicate = require('../middlewares/VerifyDuplicateEmailMiddleware');
 const authorize = require('../middlewares/AuthorizeMiddleware');
 
@@ -8,7 +9,9 @@ module.exports = (app) => {
     router.post("/signin", auth.signin);
     router.post("/refresh-tokens", auth.refreshTokens);
     router.post("/logout", [authorize.check], auth.logout); // Must be check Authorize user.
-    router.post("/auth/restore-password", auth.restorePassword);
-    router.post("/auth/confirm-restore-password", auth.confirmRestorePassword);
+    router.post("/restore-password", auth.restorePassword);
+    router.post("/confirm-restore-password", auth.confirmRestorePassword);
+    // user 
+    router.get("/users", user.users);
     app.use('/api/auth', router);
 }
