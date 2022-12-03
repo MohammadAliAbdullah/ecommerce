8d6df74ac2791e775bd7bbaad7a152b580c81f44
maabdullah511 <maabdullah511@gmail.com>
2022-12-04 00:00:43 +0600

    modify user index js

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,8 +1,12 @@
-// user controller
 const db = require('../models');
 const User = db.user;
 const Role = db.role;
 
+/*
+* @Mohammad Ali Abdullah
+* @function show all users api
+* @date 03-12-2022
+*/
 exports.users = (req, res) => {
     User.find({}, function (err, result) {
         if (err) throw new err();
